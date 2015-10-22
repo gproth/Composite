c70a43a34d6f3d3e080591d28ce7ae5ba071d88d
unknown <groth@CARPMAC-11.fs.elon.edu>
2015-10-22 14:06:09 -0400

    project created

---
+++ CarComposite/src/edu/elon/composite/CarComponent.java
@@ -0,0 +1,11 @@
+package edu.elon.composite;
+
+public abstract class CarComponent {
+
+	
+	public void add(CarComponent c){
+		throw new UnsupportedOperationException();
+	}
+	
+	public void remove(CarComponet c)[]
+}
